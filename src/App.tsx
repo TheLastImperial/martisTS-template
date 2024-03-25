8dec31e0faa8c82b58fb91dc4cc8696617d52df2
thelastimperial <thelastimperial@thelastimperial.com>
2024-03-24 23:25:54 -0700

    Theme added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import './App.css'
-import { CssBaseline, StyledEngineProvider, ThemeProvider, createTheme } from '@mui/material'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from './auth/pages/Login';
+import ThemeCustomization from './themes';
 
 function App() {
-  const themes = createTheme({});
   return (
     <>
       <BrowserRouter>
-        <StyledEngineProvider injectFirst>
-          <ThemeProvider theme={themes}>
-            <CssBaseline />
-            <Routes>
-              <Route path="/" element={ <Login/> }/>
-            </Routes>
-          </ThemeProvider>
-        </StyledEngineProvider>
+        <ThemeCustomization>
+          <Routes>
+            <Route path="/" element={ <Login/> }/>
+          </Routes>
+        </ThemeCustomization>
       </BrowserRouter>
     </>
   )
--- src/auth/pages/Tmp.tsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from '../components'
-
-export const Tmp = () => {
-  return (
-    <>
-      <LoginForm />
-    </>
-  )
-}
